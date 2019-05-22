06253b2907e10e104894c8fb9d20d3516b55441c
wzy <1084429061@qq.com>
2019-05-22 16:22:44 +0800

    上传狗粮神器

--- app/src/main/java/com/example/love_activity/ImageGlideView.java
+++ app/src/main/java/com/example/love_activity/ImageGlideView.java
@@ -39,17 +39,15 @@ public class ImageGlideView extends LinearLayout {
             final LinearLayout linearLayout = new LinearLayout(getContext());
             addView(linearLayout);
             for (int j = 0; j < 22; j++) {
-
-
                 final ImageView imageView = new ImageView(getContext());
                 imageView.setImageBitmap(readBitMap(getContext(), R.drawable.taoxin));
+
                 linearLayout.addView(imageView);
+//                scal(imageView);
                 final int finalJ = j;
                 imageView.setOnClickListener(new OnClickListener() {
                     @Override
                     public void onClick(View v) {
-
-
                         imageView.setImageBitmap(readBitMap(getContext(), image[finalJ % 6]));
                         if (finalJ == 7 || finalJ == 19) {
                             ObjectAnimator line_xuanzhuan_line = ObjectAnimator.ofFloat(linearLayout, "rotationY", 0, 360f);
@@ -80,8 +78,7 @@ public class ImageGlideView extends LinearLayout {
                                     mp.release();
                                 }
                             });
-                        }
-                        else {
+                        } else {
                             mediaPlayer = new MediaPlayer();
                             mediaPlayer = MediaPlayer.create(getContext(), R.raw.pp);
                             mediaPlayer.start();
@@ -97,23 +94,7 @@ public class ImageGlideView extends LinearLayout {
 //                        soundPool.play(load, 1, 1, 0, 0, 1);
 
 
-                            ObjectAnimator objectAnimator_rotationY = ObjectAnimator.ofFloat(imageView, "rotationY", 0, 360f);
-                            ObjectAnimator objectAnimator_transY = ObjectAnimator.ofFloat(imageView, "translationY", 0, -10, 0);
-                            ObjectAnimator objectAnimator_scaleX = ObjectAnimator.ofFloat(imageView, "scaleX", 0, 2, 1);
-                            ObjectAnimator objectAnimator_scaleY = ObjectAnimator.ofFloat(imageView, "scaleY", 0, 2, 1);
-                            objectAnimator_scaleX.setDuration(1000);
-//                        objectAnimator_scaleX.setRepeatCount(1);
-                            objectAnimator_scaleX.start();
-                            objectAnimator_scaleY.setDuration(1000);
-//                        objectAnimator_scaleY.setRepeatCount(1);
-                            objectAnimator_scaleY.start();
-
-                            objectAnimator_rotationY.setDuration(1000);
-//                        objectAnimator_rotationY.setRepeatCount(1);
-                            objectAnimator_rotationY.start();
-                            objectAnimator_transY.setDuration(1000);
-//                        objectAnimator_transY.setRepeatCount(1);
-                            objectAnimator_transY.start();
+                            normol(imageView);
                         }
 
                     }
@@ -127,6 +108,26 @@ public class ImageGlideView extends LinearLayout {
 
     }
 
+    private void normol(ImageView imageView) {
+        ObjectAnimator objectAnimator_rotationY = ObjectAnimator.ofFloat(imageView, "rotationY", 0, 360f);
+        ObjectAnimator objectAnimator_transY = ObjectAnimator.ofFloat(imageView, "translationY", 0, -10, 0);
+        ObjectAnimator objectAnimator_scaleX = ObjectAnimator.ofFloat(imageView, "scaleX", 0, 2, 1);
+        ObjectAnimator objectAnimator_scaleY = ObjectAnimator.ofFloat(imageView, "scaleY", 0, 2, 1);
+        objectAnimator_scaleX.setDuration(1000);
+//                        objectAnimator_scaleX.setRepeatCount(1);
+        objectAnimator_scaleX.start();
+        objectAnimator_scaleY.setDuration(1000);
+//                        objectAnimator_scaleY.setRepeatCount(1);
+        objectAnimator_scaleY.start();
+
+        objectAnimator_rotationY.setDuration(1000);
+//                        objectAnimator_rotationY.setRepeatCount(1);
+        objectAnimator_rotationY.start();
+        objectAnimator_transY.setDuration(1000);
+//                        objectAnimator_transY.setRepeatCount(1);
+        objectAnimator_transY.start();
+    }
+
     public static Bitmap readBitMap(Context context, int resId) {
         BitmapFactory.Options opt = new BitmapFactory.Options();
         opt.inPreferredConfig = Bitmap.Config.RGB_565;
@@ -135,5 +136,17 @@ public class ImageGlideView extends LinearLayout {
         //获取资源图片
         InputStream is = context.getResources().openRawResource(resId);
         return BitmapFactory.decodeStream(is, null, opt);
+    }
+
+
+    private void scal(ImageView imageView) {
+        ObjectAnimator doudong = ObjectAnimator.ofFloat(imageView, "scaleX", 1,2, 1);
+        doudong.setRepeatCount(1000000000);
+        doudong.setDuration(10);
+        doudong.start();
+        ObjectAnimator doudongY = ObjectAnimator.ofFloat(imageView, "scaleY", 1, 2, 1);
+        doudongY.setRepeatCount(1000000000);
+        doudongY.setDuration(10);
+        doudongY.start();
     }
 }
